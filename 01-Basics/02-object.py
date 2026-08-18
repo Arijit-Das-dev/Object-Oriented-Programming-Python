4e0d83d31643560c3c2b556f714ec93cd1a37287
Arijit Das <arijitdas.das99@gmail.com>
2026-08-18 23:25:13 +0530

    updated class & objects

--- 01-Basics/02-object.py
+++ 01-Basics/02-object.py
@@ -1,6 +1,5 @@
 # Creating class blueprint
 # This is a class or a blueprint of a student
-
 class Student:
 
     name: str = 'Devid'
@@ -10,6 +9,12 @@ class Student:
     is_student: bool = True
 
 # creating objects of that student class
-
 obj_1 = Student()   # object 1 -> Student class 
 obj_2 = Student()   # object 2 -> Student class
+
+# All the functionalities of that class
+print(obj_1.name)
+print(obj_1.dept)
+print(obj_1.sec)
+print(obj_1.age)
+print(obj_1.is_student)
