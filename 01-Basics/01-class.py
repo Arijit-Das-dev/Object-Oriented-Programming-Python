4acbaaa7ea9d52853b9978b4009de8d0ece38e1f
Arijit Das <arijitdas.das99@gmail.com>
2026-08-18 23:22:49 +0530

    01 basics

---
+++ 01-Basics/01-class.py
@@ -0,0 +1,10 @@
+# Creating class blueprint
+class Student:
+
+    name: str = 'Devid'
+    dept: str = 'CSE'
+    sec: str = 'X'
+    age: int = 18
+    is_student: bool = True
+
+# This is a class or a blueprint of a student
---
+++ 01-Basics/02-object.py
@@ -0,0 +1,15 @@
+# Creating class blueprint
+# This is a class or a blueprint of a student
+
+class Student:
+
+    name: str = 'Devid'
+    dept: str = 'CSE'
+    sec: str = 'X'
+    age: int = 18
+    is_student: bool = True
+
+# creating objects of that student class
+
+obj_1 = Student()   # object 1 -> Student class 
+obj_2 = Student()   # object 2 -> Student class
